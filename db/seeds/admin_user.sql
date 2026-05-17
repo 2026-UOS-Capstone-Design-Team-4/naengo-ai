c8f3ac28f2ec2c3951d33096d44c11141d071f3c
yschoi <yschoi9733@gmail.com>
2026-05-18 07:50:02 +0900

    chore: admin_user.sql 제거

--- db/seeds/admin_user.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO users (email, password_hash, nickname, role, is_blocked)
-VALUES ('yschoi9733@gmail.com', '1234', '냉고', 'ADMIN', false)
-ON CONFLICT (email) DO NOTHING;
-
-INSERT INTO user_profiles (user_id, user_input)
-VALUES (1, '[]')
-ON CONFLICT (user_id) DO NOTHING;
