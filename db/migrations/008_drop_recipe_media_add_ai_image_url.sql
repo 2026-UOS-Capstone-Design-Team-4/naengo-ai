c3efd5a6a82acc2bc46305908c854844c91dd6b1
yschoi <yschoi9733@gmail.com>
2026-05-23 02:58:01 +0900

    chore: recipe media 마이그레이션 제거

--- db/migrations/008_drop_recipe_media_add_ai_image_url.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE recipes ADD COLUMN ai_main_image_url VARCHAR(1024);
-ALTER TABLE recipe_steps ADD COLUMN ai_image_url VARCHAR(1024);
-
-DROP TABLE IF EXISTS recipe_image_generations CASCADE;
-DROP TABLE IF EXISTS recipe_media CASCADE;
--- db/migrations/011_rename_user_recipes_main_image_url.sql
+++ db/migrations/011_rename_user_recipes_main_image_url.sql
@@ -1,5 +1,2 @@
 ALTER TABLE user_recipes
 RENAME COLUMN source_main_image_url TO main_image_url;
-
-ALTER TABLE user_recipes_steps
-RENAME COLUMN sourceimage_url TO image_url;
